6a32c529265c2464b738088c811d6812cee5d5e8
Parthiban <parthi.s1805@gmail.com>
2024-08-24 11:48:42 +0530

    not_found_page_update

--- event/src/components/notfound/notfound.jsx
+++ event/src/components/notfound/notfound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
+    <div style={{  textAlign: 'center', padding: '50px', height:"100vh", display:"flex", flexDirection:"column", alignItems:"center",justifyContent:"center" }}>
       <h1>404 - Not Found</h1>
       <p>The page you're looking for does not exist.</p>
     </div>
