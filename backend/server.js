4294d3afe72a5a1d048d6726b7686118e44c8fc4
Parthiban <parthi.s1805@gmail.com>
2024-08-27 12:14:50 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -79,7 +79,7 @@ app.post('/register', upload.single('paymentScreenshot'), async (req, res) => {
     blood: req.body.bloodGroup,
     hORd: req.body.hORd,
     hostelID: req.body.hostelID,
-    paymentScreenshot: req.file ? req.file.path : null,
+    paymentScreenshot: `/uploads/${req.file.filename}`,  
   };
 
   try {
