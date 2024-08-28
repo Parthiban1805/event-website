a6e79f6d4f7d819b2e20ef250486acafac5e9ffb
Parthiban <parthi.s1805@gmail.com>
2024-08-28 10:43:25 +0530

    server_update

--- backend/server.js
+++ backend/server.js
@@ -117,6 +117,12 @@ app.post('/register', upload.single('paymentScreenshot'), async (req, res) => {
         hORd: req.body.hORd,
         hostelNo: req.body.hostelNo,
         paymentScreenshot: req.file ? req.file.path : null,
+        registerType: req.body.registerType,
+      promotionDetails: req.body.registerType === "promotion" ? req.body.promotionDetails : undefined,
+      promotionDetailsPerson: req.body.registerType === "promotion" ? req.body.promotionDetailsPerson : undefined,
+      individualPerson: req.body.registerType === "individual" ? req.body.individualPerson : undefined,
+      helpDeskOption: req.body.registerType === "help_desk" ? req.body.helpDeskOption : undefined,
+   
     };
 
     try {
