493d81ec2b4dada626b72cb01d095109c9e19d90
Parthiban <parthi.s1805@gmail.com>
2024-09-12 13:43:01 +0530

    sponsor logo update

--- event/src/components/scrollingcards/scrollingcards.jsx
+++ event/src/components/scrollingcards/scrollingcards.jsx
@@ -3,6 +3,7 @@ import decathlon from '../../assets/sponsor-logos/decathlonlogo.png';
 import gatorade from '../../assets/sponsor-logos/gatoradelogo.png';
 import livn from '../../assets/sponsor-logos/livn.png';
 import who from '../../assets/sponsor-logos/whologo.png';
+import skechers from '../../assets/sponsor-logos/skechers.png'
 import './scrollingcards.css';
 
 const ScrollingCards = () => {
@@ -12,20 +13,24 @@ const ScrollingCards = () => {
         <img src={decathlon} alt="Decathlon" />
         <img src={gatorade} alt="Gatorade" />
         <img src={livn} alt="Livn" />
+        <img src={skechers} alt="skechers" />
         <img src={who} alt="WHO" />
         <img src={decathlon} alt="Decathlon" />
         <img src={gatorade} alt="Gatorade" />
         <img src={livn} alt="Livn" />
+        <img src={skechers} alt="skechers" />
         <img src={who} alt="WHO" />
       </div>
       <div className="logos-slide">
         <img src={decathlon} alt="Decathlon" />
         <img src={gatorade} alt="Gatorade" />
         <img src={livn} alt="Livn" />
+        <img src={skechers} alt="skechers" />
         <img src={who} alt="WHO" />
         <img src={decathlon} alt="Decathlon" />
         <img src={gatorade} alt="Gatorade" />
         <img src={livn} alt="Livn" />
+        <img src={skechers} alt="skechers" />
         <img src={who} alt="WHO" />
       </div>
     </div>
